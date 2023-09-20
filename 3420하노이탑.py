bdb588323ad1f0544ddd275534cd17a0621b8362
akns27 <pkw072@gmail.com>
2023-09-21 08:06:38 +0900

    0921

---
+++ 3420하노이탑.py
@@ -0,0 +1,11 @@
+def hanoi(n, a, b, c):#재귀함수 hanoi(n, a, b, c)를 지정
+    if n == 1:#n이 1개 즉 남은 원판이 하나 일때
+        print(f"Disk {n} : {a} to {c}")#a to b로 n원판을 이동한 경로
+    else:#남은 원판이 하나가 아니면
+        hanoi(n-1,a,c,b)  # c를 이용해서 a를 b로 이동
+        print(f"Disk {n} : {a} to {c}")#a to c로 n원판을 이동한 경로
+        hanoi(n-1,b,a,c)#a를 이용해서 b를 c로 이동
+#n-1을 쓰는 이유는 1개인 경우는 if문으로 빠지고 2개 이상인 경우 제일 위에서 아래에 있는 원판을 n-1 지정해주는 것
+if __name__ == "__main__":#이 아래 코드는 직접 실행 할때만 실행되게 하겠다.
+    n = int(input())#입력
+    hanoi(n,'A','B','C')
--- 바둑판_리스트컴프리헨션(박).py
+++ 바둑판_리스트컴프리헨션(박).py
@@ -22,27 +22,23 @@ for i in range(1, 20) :
 1. 0이 들어간 바둑판 만들기 -> []으로 리스트를 일단 만들고 []안에 append를 써서 리스트를 넣고, 열과 행을 만들고(20X20) 0을 넣기
 2. 좌표값 입력 받아서 1 넣기 -> x,y 입력받고, 입력받은 게 있는거에 1로 바꿔주기
 3. 사각형 모양 출력하기 -> 이중 for문을 써서 출력하기
-"""
 
-"""
 아래에는 리스트 컴프리헨션을 이용하여 업그레이드 한 코드
 
-  # 20x20 크기의 2차원 리스트 생성 및 0으로 초기화
-d = [[0] * 20 for _ in range(20)]
-
+"""
 # 돌의 개수 n 입력받기
 n = int(input())
+ # 20x20 크기의 2차원 리스트 생성 및 0으로 초기화
+d = [[0 for i in range(20)] for j in range(20)]#[0]짜리 칸을 20번 만듦
+
 
 # 돌을 놓을 좌표 입력받고 바둑판에 1로 표시
-for _ in range(n):
-    x, y = map(int, input().split())
-    d[x][y] = 1
+for i in range(n):
+    x,y = map(int,input().split())
+    d[x][y] = 1 
 
-# 바둑판 출력
-for i in range(1, 20):
-    for j in range(1, 20):
+ # 바둑판 출력
+for i in range(1,20):
+    for j in range(1,20):
         print(d[i][j], end=' ')
     print()
-
-"""
-
